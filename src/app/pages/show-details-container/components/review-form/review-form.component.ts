731b2e0652c64c48b5b9cb65c35213eb35b31feb
unknown <kutnjakantonio@gmail.com>
2021-07-27 08:10:30 +0200

    Added star rating

--- src/app/pages/show-details-container/components/review-form/review-form.component.ts
+++ src/app/pages/show-details-container/components/review-form/review-form.component.ts
@@ -11,14 +11,33 @@ import { IRawReview } from 'src/app/interfaces/rawReview.interface';
 export class ReviewFormComponent {
 	@Output() submit: EventEmitter<IRawReview> = new EventEmitter();
 
+	private rating = 0;
+	private starCount = 5;
+	public ratingArr: boolean[] = [];
+
+	constructor(private fb: FormBuilder) {
+		this.ratingArr = Array(this.starCount).fill(false);
+	}
+
 	public reviewFormGroup: FormGroup = this.fb.group({
 		comment: ['', [Validators.required]],
-		rating: ['', [Validators.required, Validators.min(1), Validators.max(5)]],
+		rating: [''],
 	});
 
-	constructor(private fb: FormBuilder) {}
+	public getStar(i: number) {
+		if (this.rating >= i + 1) {
+			return 'star';
+		} else {
+			return 'star_border';
+		}
+	}
+
+	public onStarClick(i: number) {
+		this.rating = i + 1;
+	}
 
 	public onSubmit(): void {
+		this.reviewFormGroup.patchValue({ rating: `${this.rating}` });
 		this.submit.emit(this.reviewFormGroup.value);
 		this.reviewFormGroup.reset();
 	}
