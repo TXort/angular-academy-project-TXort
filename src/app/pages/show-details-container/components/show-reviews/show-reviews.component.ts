0301c776b95c65673a0cf21dc67f141325078346
unknown <kutnjakantonio@gmail.com>
2021-07-27 14:56:44 +0200

    Added part of delete review function

--- src/app/pages/show-details-container/components/show-reviews/show-reviews.component.ts
+++ src/app/pages/show-details-container/components/show-reviews/show-reviews.component.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 import { Review } from 'src/app/services/review.model';
+import { IUser } from 'src/app/interfaces/user.interface';
 
 @Component({
 	selector: 'app-show-reviews',
@@ -9,4 +11,13 @@ import { Review } from 'src/app/services/review.model';
 })
 export class ShowReviewsComponent {
 	@Input() reviews: Array<Review>;
+
+	constructor(private authService: AuthService) {}
+
+	public canDelete(review: Review): string {
+		if (this.authService.getAuthData()?.uid === review.user.email) return 'enabled';
+		return 'disabled';
+	}
+
+	public onDelete(review: Review) {}
 }
--- src/app/services/review.model.ts
+++ src/app/services/review.model.ts
@@ -1,4 +1,5 @@
 import { IRawReview } from '../interfaces/rawReview.interface';
+import { IUser } from '../interfaces/user.interface';
 
 export class Review {
 	constructor(rawReviewData: IRawReview) {
@@ -6,10 +7,12 @@ export class Review {
 		this.comment = rawReviewData.comment;
 		this.show_id = rawReviewData.show_id;
 		this.id = rawReviewData.id;
+		this.user = rawReviewData.user;
 	}
 
 	rating: number;
 	comment: string;
 	show_id: string;
 	id: string;
+	user: IUser;
 }
