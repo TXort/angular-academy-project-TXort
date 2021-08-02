1341d208a26c033105e99bb732bddc59738dda4e
unknown <kutnjakantonio@gmail.com>
2021-08-03 01:07:48 +0200

    Fix main layout bug

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+
+interface ILayout {
+	isSmall: boolean;
+}
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.css']
+  styleUrls: ['./main-layout.component.css'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+export class MainLayoutComponent {
 
-  ngOnInit(): void {
-  }
+  public layout$: Observable<ILayout>;
+
+
+	constructor(breakpointObserver: BreakpointObserver) {
+		this.layout$ = breakpointObserver.observe(['(min-width: 0px) and (max-width: 767px)', '(max-width: 768px)']).pipe(
+			map(({ matches }) => {
+				return {
+					isSmall: matches,
+				};
+			})
+		);
+	}
+
 
 }
